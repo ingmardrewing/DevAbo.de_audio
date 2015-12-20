2e8590889b659709a0ad9a0967d4bc08ab7d4996
Ingmar Drewing <ingmar@drewing.de>
2015-12-20 12:20:53 +0100

    empty tracklist added

---
+++ DevAbo.de/Tracklist.h
@@ -0,0 +1,11 @@
+//
+//  Tracklist.h
+//  DevAbo.de
+//
+//  Created by Ingmar Drewing on 20/12/15.
+//  Copyright © 2015 Ingmar Drewing. All rights reserved.
+//
+
+@interface Tracklist 
+
+@end
---
+++ DevAbo.de/Tracklist.m
@@ -0,0 +1,20 @@
+//
+//  Tracklist.m
+//  DevAbo.de
+//
+//  Created by Ingmar Drewing on 20/12/15.
+//  Copyright © 2015 Ingmar Drewing. All rights reserved.
+//
+
+#import "Tracklist.h"
+#import <Foundation/Foundation.h>
+
+@interface Tracklist (){ }
+
+@end
+
+@implementation Tracklist
+
+
+
+@end
