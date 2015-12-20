40149516906c8fc97b9c73cec8794284c046f758
Ingmar Drewing <ingmar@drewing.de>
2015-12-20 17:43:41 +0100

    Foreground / Background-Management for location

--- DevAbo.de/ViewController.h
+++ DevAbo.de/ViewController.h
@@ -13,7 +13,8 @@
 @interface ViewController : UIViewController <CLLocationManagerDelegate>
 
 - (IBAction)Play:(id)sender;
-- (IBAction)locate:(id)sender;
+- (void) setForeground;
+- (void) setBackground;
 
 @end
 
--- DevAbo.de/ViewController.m
+++ DevAbo.de/ViewController.m
@@ -21,27 +21,49 @@
 
 @implementation ViewController
 
+- (void) setForeground {
+    NSLog(@"setForeground");
+    [locationManager stopMonitoringSignificantLocationChanges];
+    [locationManager startUpdatingLocation];
+}
+
+- (void) setBackground {
+    NSLog(@"setBackground");
+    [locationManager stopUpdatingLocation];
+    [locationManager startMonitoringSignificantLocationChanges];
+}
+
 - (IBAction)Play:(id)sender {
     NSLog(@"pushed play");
     [_audioPlayer play];
 }
 
-- (IBAction)locate:(id)sender {
-    NSLog(@"pushed location");
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
+    NSLog(@"locationManager called");
+
     float Lat = locationManager.location.coordinate.latitude;
     float Long = locationManager.location.coordinate.longitude;
     NSLog(@"Lat : %f  Long : %f",Lat,Long);
 }
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+
     
-    //  locationServicesEnabled class method of CLLocationManager
+    // Start location services
     locationManager = [[CLLocationManager alloc] init];
+    locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    
+    // Only report to location manager if the user has traveled 1000 meters
     locationManager.distanceFilter = kCLDistanceFilterNone;
-    locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters;
-    [locationManager startUpdatingLocation];
+    locationManager.desiredAccuracy =kCLLocationAccuracyHundredMeters;
+    locationManager.delegate = self;
+    locationManager.activityType = CLActivityTypeFitness;
+    
     [locationManager requestAlwaysAuthorization];
+    [locationManager startMonitoringSignificantLocationChanges];
+//    [locationManager startUpdatingLocation];
 
 
     // Create tracklist, thus allowing the audioplayer to get soundUrls
